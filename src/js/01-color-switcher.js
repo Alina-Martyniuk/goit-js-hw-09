c63dab751f6ee2e941f3709a3cd17adfceac7555
Alina <alina.martyniuk.biz@gmail.com>
2022-12-29 17:06:01 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+let intervalId;
+
+
+startBtn.addEventListener("click", startChangeColor);
+stopBtn.addEventListener("click", stopChangeColor);
+
+function startChangeColor() {
+    startBtn.setAttribute("disabled", "disabled");
+    stopBtn.removeAttribute("disabled");
+    intervalId = setInterval(() => body.style.backgroundColor = getRandomHexColor(), 1000);
+    return intervalId
+
+}
+
+function stopChangeColor() {
+    stopBtn.setAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled");
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
